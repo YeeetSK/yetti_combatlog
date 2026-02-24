1f1eae1d323dc96cc80329c9c38842af2291c603
Yeet <113854826+YeeetSK@users.noreply.github.com>
2026-02-24 21:58:22 +0100

    add missing bridge

--- bridge/server/esx.lua
+++ bridge/server/esx.lua
@@ -1,6 +1,65 @@
 if Config.Framework ~= "esx" then return end
 local ESX = exports['es_extended']:getSharedObject()
 
-function Notification(text, type)
-    ESX.ShowNotification(text, type)
+function GetCharName(src)
+    local xPlayer = ESX.GetPlayerFromId(src)
+    return xPlayer.getName()
 end
+
+function GetPlyIdentifier(src)
+    local xPlayer = ESX.GetPlayerFromId(src)
+    return xPlayer.identifier
+end
+
+function GetSkinData(identifier)
+    local result = MySQL.single.await('SELECT skin FROM users WHERE identifier = ?', {
+        identifier
+    })
+    if result and result.skin then
+        local skinData = json.decode(result.skin)
+        if skinData then
+            return skinData, skinData.model
+        end
+    end
+    return false
+end
+
+function GetAllPlayersData()
+    local xPlayers = ESX.ExtendedPlayers()
+
+    if xPlayers then
+        local playerData = {}
+        for i, xPlayer in ipairs(xPlayers) do
+            local src = tonumber(xPlayer.source)
+            if not src then return end
+            playerData[src] = {
+                name = GetCharName(src),
+                identifier = GetPlyIdentifier(src),
+                license = GetPlayerIdentifierByType(src, 'license')
+            }
+        end
+        return playerData
+    else
+        if Config.Debug then
+            print("No players online, script restarted")
+            return
+        end
+    end
+end
+
+function GetPlayerInventory(identifier)
+    local result = MySQL.single.await('SELECT inventory FROM players WHERE citizenid = ?', {
+        identifier
+    })
+    return result.inventory
+end
+
+function SetPlayerInventory(identifier, inventory)
+    MySQL.update.await('UPDATE users SET inventory = ? WHERE identifier = ?', {
+        inventory, identifier
+    })
+end
+
+AddEventHandler('esx:playerLoaded', function(source)
+    OnPlayerLoaded(source)
+end)
--- bridge/server/qb.lua
+++ bridge/server/qb.lua
@@ -1,6 +1,68 @@
 if Config.Framework ~= "qb" then return end
 
+local QBCore = exports['qb-core']:GetCoreObject()
 
+function GetCharName(src)
+    local Player = QBCore.Functions.GetPlayer(src)
+    local charinfo = Player.PlayerData.charinfo
+    local name = charinfo.firstname .. ' ' .. charinfo.lastname
 
+    return name
+end
 
+function GetPlyIdentifier(src)
+    local Player = QBCore.Functions.GetPlayer(src)
+    return Player.PlayerData.citizenid
+end
 
+function GetSkinData(identifier)
+    local result = MySQL.single.await('SELECT skin FROM playerskins WHERE citizenid = ? AND active = ?', {
+        identifier, 1
+    })
+    if result and result.skin then
+        local skinData = json.decode(result.skin)
+        if skinData then
+            return skinData, skinData.model
+        end
+    end
+    return false
+end
+
+function GetAllPlayersData()
+    local players = QBCore.Functions.GetQBPlayers()
+
+    if players then
+        local playerData = {}
+        for src, data in pairs(players) do
+            if not src then return end
+            playerData[src] = {
+                name = GetCharName(src),
+                identifier = data.PlayerData.citizenid,
+                license = GetPlayerIdentifierByType(src, 'license')
+            }
+        end
+        return playerData
+    else
+        if Config.Debug then
+            print("No players online, script restarted")
+            return
+        end
+    end
+end
+
+function GetPlayerInventory(identifier)
+    local result = MySQL.single.await('SELECT inventory FROM players WHERE citizenid = ?', {
+        identifier
+    })
+    return result.inventory
+end
+
+function SetPlayerInventory(identifier, inventory)
+    MySQL.update.await('UPDATE players SET inventory = ? WHERE citizenid = ?', {
+        inventory, identifier
+    })
+end
+
+RegisterNetEvent('QBCore:Server:OnPlayerLoaded', function()
+    OnPlayerLoaded(source)
+end)
